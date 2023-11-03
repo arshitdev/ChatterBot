a49b5e7341799b51a3b03e5a30a97418683ad071
arshitdev <149812109+arshitdev@users.noreply.github.com>
2023-11-04 01:16:03 +0530

    add code for english training in comment

--- app/train.py
+++ app/train.py
@@ -6,6 +6,10 @@ chatbot = ChatBot('mybot')
 
 wiki_trainer = WikiTrainer(chatbot)
 
+# train on english corpus data
+# wiki_trainer.train('D:\\work\\english.tsv')
+
+
 wiki_trainer.train('D:\\work\\wiki.tsv')
 
 print('Training Complete')
